27e1941060101a5fb9043fd03fe795ec805e53a7
godwinnwalozie <godwin.nwalozie@gmai.com>
2022-08-16 08:25:10 +0100

    update streamlit version

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
-from ipywidgets import interact
 from sklearn.preprocessing import StandardScaler
 import os
 import random
